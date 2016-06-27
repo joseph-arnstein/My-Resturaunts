98fd20979656c10bf62a4631b526760cd3bda956
Joey Arnstein and Peter Beach <joey@>
2016-06-27 13:34:59 -0700

    added unit and integration testing

---
+++ app/src/androidTest/java/com/breakstuff/myrestaurants/MainActivityInstrumentationTest.java
@@ -0,0 +1,35 @@
+package com.breakstuff.myrestaurants;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.editText)).perform(typeText("Portland"))
+                .check(matches(withText("Portland")));
+    }
+    @Test
+    public void locationIsSentToRestaurantsActivity() {
+        String location = "Portland";
+        onView(withId(R.id.editText)).perform(typeText(location));
+        onView(withId(R.id.findRestaurantsButton)).perform(click());
+        onView(withId(R.id.textView)).check(matches
+                (withText("Here are all the restaurants near: " + location)));
+    }
+
+}
