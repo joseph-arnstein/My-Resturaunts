1c52c4cc877166631ccd02f0c3ff2c0e8972162a
Joey <joeyarnstein@gmail.com>
2016-07-06 09:24:25 -0700

    add dependencies to build in preparation for implementing api calls

---
+++ app/src/main/java/com/breakstuff/myrestaurants/Constants.java
@@ -0,0 +1,11 @@
+package com.breakstuff.myrestaurants;
+
+/**
+ * Created by AlinaAir on 7/6/16.
+ */
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
